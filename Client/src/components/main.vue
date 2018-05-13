64a613b0ae9f79a55ac2dee565fe3d658a885973
Marcel <m.brodbeck89@gmail.com>
2018-05-13 19:43:06 +0200

    Time Picker und Selects eingefügt

--- Client/src/components/main.vue
+++ Client/src/components/main.vue
@@ -1,15 +1,6 @@
 <template>
   <div>
-      
-
-      <img  class="displayed" src="../assets/bechtle1.jpg">
-       <br>
-      <v-container grid-list-md text-xs-center>
-      <v-layout row >
-      <v-flex xs10>
     
-     
-      
       <v-dialog v-model="dialog" max-width="500px">
       <v-btn slot="activator" color="primary" dark class="mb-2">New Item</v-btn>
       <v-card>
@@ -20,7 +11,32 @@
           <v-container grid-list-md>
             <v-layout wrap>
               <v-flex xs12 sm6 md4>
-                <v-text-field v-model="editedItem.datum" label="Datum"></v-text-field>
+                  <v-menu
+        ref="menu"
+        :close-on-content-click="false"
+        v-model="menu"
+        :nudge-right="40"
+        :return-value.sync="editedItem.datum"
+        lazy
+        transition="scale-transition"
+        offset-y
+        full-width
+        min-width="290px"
+      >
+        <v-text-field
+          slot="activator"
+          v-model="editedItem.datum"
+          label="Datum auswählen"
+          prepend-icon="event"
+          readonly
+        ></v-text-field>
+        <v-date-picker locale="de" v-model="editedItem.datum" no-title scrollable>
+          <v-spacer></v-spacer>
+          <v-btn flat color="primary" @click="menu = false">Cancel</v-btn>
+          <v-btn flat color="primary" @click="$refs.menu.save(editedItem.datum)">OK</v-btn>
+        </v-date-picker>
+      </v-menu>
+
               </v-flex>
               <v-flex xs12 sm6 md4>
                 <v-text-field v-model="editedItem.von" label="Arbeitsbeginn"></v-text-field>
@@ -29,10 +45,11 @@
                 <v-text-field v-model="editedItem.bis" label="ArbeitsEnde"></v-text-field>
               </v-flex>
               <v-flex xs12 sm6 md4>
-                <v-text-field v-model="editedItem.SL" label="ServiceLeistung"></v-text-field>
+                <v-select :items="Serviceleistung" v-model="editedItem.SL" label="ServiceLeistung"></v-select>
               </v-flex>
               <v-flex xs12 sm6 md4>
-                <v-text-field v-model="editedItem.LS" label="Leistungsschein"></v-text-field>
+                <v-select :items="Leistungsschein" v-model="editedItem.SL" label="Leistungsschein"></v-select>
+                <v-select :items="Arbeitsort" v-model="editedItem.SL" label="Arbeitsort"></v-select>
               </v-flex>
             </v-layout>
           </v-container>
@@ -71,110 +88,122 @@
         <v-btn color="primary" @click="initialize">Reset</v-btn>
       </template>
     </v-data-table>
-     
-      </v-flex>  
-    </v-layout>
-       </v-container>
+          
   </div>
 </template>
 <script>
 export default {
-    data: () => ({
-      dialog: false,
-      
-      headers: [
-        {
-          text: 'Datum',
-          align: 'left',
-          
-          value: 'datum'
-        },
-        { text: 'Arbeitsbeginn', value: 'von', sortable: false, },
-        { text: 'ArbeitsEnde', value: 'bis', sortable: false, },
-        { text: 'Serviceleistung', value:'SL', sortable: false, },
-        { text: 'Leistungsschein',value:'LS'  },
+  data: () => ({
+    dialog: false,
+    menu: false,
+
+    headers: [
+        { text: 'Datum',align: 'left',value: 'datum'},
+        { text: 'Arbeitsbeginn', value: 'von', sortable: false },
+        { text: 'ArbeitsEnde', value: 'bis', sortable: false },
+        { text: 'Serviceleistung', value: 'SL', sortable: false },
+        { text: 'Leistungsschein', value: 'LS' },
+        { text: 'ArbeitsOrt', value:'Ort'},
         { text: 'Actions', value: 'name', sortable: false }
-      ],
-      desserts: [],
-      editedIndex: -1,
-      editedItem: {
-        datum: '',
-        von: '',
-        bis: '',
-        SL: '',
-        LS: ''
-      },
-      defaultItem: {
-        datum: '',
-        von: '',
-        bis: '',
-        SL: '',
-        LS: ''
-      }
-    }),
-
-    computed: {
-      formTitle () {
-        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
-      }
+            ],
+    Leistungsschein: [
+'LS01-AK-Beschaffung',
+'LS02-AK-Lager & Logistik, Installation',
+'LS03-AK Rollout',
+'LS04-AK-Incident & VPN Token',
+'LS05-AK MAC OS X',
+'LS06-AK-Mobile Devices & Asset MGMT.',
+'LS07-AK-Produktion',
+'LS08-AK-Ersatzteile & Reperaturen',
+'LS09-AK-Sonderaufträge',
+'LS10-AK-Produktion & Testing'],
+    Serviceleistung:[
+'Service Techniker',
+'Service Techniker ""B""',
+'System Engineer',
+'Repräsentant / Projektleiter',
+'Senior Engineer / Consultant',
+'Auszubildende',
+'Praktikant',
+'DB Entwicklung'],
+    Arbeitsort:['Kärcher','Heller','MHP','Mercedes-Benz-Museum'],
+    list: [],
+    editedIndex: -1,
+    editedItem: {
+      datum: null,
+      von: '',
+      bis: '',
+      SL: '',
+      LS: ''
     },
+    defaultItem: {
+      datum: null,
+      von: '',
+      bis: '',
+      SL: '',
+      LS: ''
+    }
+  }),
 
-    watch: {
-      dialog (val) {
-        val || this.close()
-      }
-    },
+  computed: {
+    formTitle () {
+      return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+    }
+  },
 
-    created () {
-      this.initialize()
-    },
+  watch: {
+    dialog (val) {
+      val || this.close()
+    }
+  },
 
-    methods: {
-      initialize () {
-        this.desserts = [
-          
-            
-          {
-            datum: '10.05.2018',
-            von: '07:30',
-            bis: '16:00',
-            SL: 'ServiceTechniker',
-            LS: 'Rollout'
-          }
-        ]
-      },
+  created () {
+    this.initialize()
+  },
 
-      editItem (item) {
-        this.editedIndex = this.desserts.indexOf(item)
-        this.editedItem = Object.assign({}, item)
-        this.dialog = true
-      },
+  methods: {
+    initialize () {
+      this.list = [
 
-      deleteItem (item) {
-        const index = this.desserts.indexOf(item)
-        confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
-      },
-
-      close () {
-        this.dialog = false
-        setTimeout(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        }, 300)
-      },
-
-      save () {
-        if (this.editedIndex > -1) {
-          Object.assign(this.desserts[this.editedIndex], this.editedItem)
-        } else {
-          this.desserts.push(this.editedItem)
+        {
+          datum: '10.05.2018',
+          von: '07:30',
+          bis: '16:00',
+          SL: 'ServiceTechniker',
+          LS: 'Rollout'
         }
-        this.close()
+      ]
+    },
+
+    editItem (item) {
+      this.editedIndex = list.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+
+    deleteItem (item) {
+      const index = this.list.indexOf(item)
+      confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
+    },
+
+    close () {
+      this.dialog = false
+      setTimeout(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      }, 300)
+    },
+
+    save () {
+      if (this.editedIndex > -1) {
+        Object.assign(this.list[this.editedIndex], this.editedItem)
+      } else {
+        this.list.push(this.editedItem)
       }
+      this.close()
     }
   }
-  
-
+}
 </script>
 <style>
 img.displayed{
