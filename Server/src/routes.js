9d273e78122568323b0768fc506fe74fe408b6cb
Brodbeck <AWX2341@de.KAERCHER.com>
2018-05-16 15:36:51 +0200

    messing around with sequelize

--- Server/src/routes.js
+++ Server/src/routes.js
@@ -1,4 +1,5 @@
 const AuthenticationController = require ('./controllers/AuthenticationContoller.js')
+const {zeitstempel} = require('./db/models')
 module.exports = (app) => {
     app.post('/register', 
         AuthenticationController.register
@@ -7,5 +8,19 @@ module.exports = (app) => {
     app.post('/login', 
         AuthenticationController.login
       
-    )
-}
+    ),
+    app.get('/db/main', function(req,res){
+        const zs = zeitstempel.findAll()   
+    }),
+
+    app.post('/db/zeitstempel', function(req,res){
+        AuthenticationController.zeitstempel
+    }
+
+    
+)
+            
+}        
+        
+      
+
