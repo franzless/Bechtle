7c5594d538c8e7417e7c6d74d27728d636dfd4f1
Brodbeck <AWX2341@AKAD.KAERCHER.COM>
2018-06-14 15:43:07 +0200

    further testing

--- Client/src/components/einsatzplanung.vue
+++ Client/src/components/einsatzplanung.vue
@@ -1,15 +1,23 @@
 <template>
 <div>
     <my-toolbar></my-toolbar>
-    <v-card height="400x" width="150">
-       <draggable v-model="users">
-          
-               <v-card v-for="user in users" :key="user.userid">
-                   <v-card-media src="http://source.unsplash.com/random/150x150" height="150px"></v-card-media>
-                    <v-card-title>
-                        {{user.firstname}} {{user.lastname}}
+    <h1>Kärcher</h1>
+    <v-card height="400px" width="1000" >
+       <draggable v-model="kärcher">
+               <v-container grid-list-xs >
+                   <v-layout wrap>
+                  
+               <v-card width="150px" v-for="k in kärcher" :key="k.name" color="blue darken-1">
+                   <v-card-title>
+                        {{k.name}}
                     </v-card-title>
+                   <v-card-media :src="k.img" height="100px"></v-card-media>
+                    <v-card-text>
+                        {{k.Information}} {{k.Dauer}}
+                    </v-card-text>
                </v-card>
+               </v-layout>
+               </v-container> 
        </draggable>             
 
 
@@ -25,6 +33,12 @@
 export default {
     data(){
         return{
+            kärcher:[
+                {name:'Marcel Brodbeck',Team:'Kärcher',Information:'Stamm',Dauer:'',img:'http://source.unsplash.com/random/150x150'},
+                {name:'Michele Grasso',Team:'Kärcher',Information:'Stamm',Dauer:'',img:'http://source.unsplash.com/random/150x150'},
+                {name:'Michael Gollhofer',Team:'Kärcher',Information:'Stamm',Dauer:'',img:'http://source.unsplash.com/random/150x150'},
+                {name:'Wolfgang Reichle',Team:'Kärcher',Information:'Temporär',Dauer:'12.6.2018-20.06.2018',img:'http://source.unsplash.com/random/150x150'},
+            ]
           
         }
     },
