eb57f75dd010f0839c7b731a7373b93fc85e70f2
Brodbeck <AWX2341@AKAD.KAERCHER.COM>
2018-06-15 15:42:17 +0200

    drag and drop testing

--- Client/src/components/einsatzplanung.vue
+++ Client/src/components/einsatzplanung.vue
@@ -1,30 +1,41 @@
 <template>
 <div>
     <my-toolbar></my-toolbar>
-    <h1>Kärcher</h1>
-    <v-card height="400px" width="1000" >
-       <draggable v-model="kärcher">
-               <v-container grid-list-xs >
-                   <v-layout wrap>
-                  
-               <v-card width="150px" v-for="k in kärcher" :key="k.name" color="blue darken-1">
-                   <v-card-title>
-                        {{k.name}}
-                    </v-card-title>
-                   <v-card-media :src="k.img" height="100px"></v-card-media>
-                    <v-card-text>
-                        {{k.Information}} {{k.Dauer}}
-                    </v-card-text>
-               </v-card>
-               </v-layout>
-               </v-container> 
-       </draggable>             
+ <v-layout row >
+     <v-flex xs12 sm6 offset-sm3>
+         <v-card class="kärcher">
+             <v-toolbar><v-avatar><img src="../assets/kärcher.png" alt=""></v-avatar><v-toolbar-title>Team Kärcher</v-toolbar-title></v-toolbar>
+           <v-container fluid grid-list-md> 
+            
+                <draggable v-model="kärcher" :element="'v-layout'" row wrap  :options="{group:'team'}">
+                <v-flex class="kärcher" v-for="k in kärcher" :key="k.name">
+                    <div class="header">{{k.name}}</div>
+                    <img :src="k.img" height="150px" width="150px">
+                    <v-flex xs1 >{{k.Information}}  {{k.Dauer}}</v-flex>
+                </v-flex>
+                 </draggable>
+             </v-container>
+         </v-card>
+         <br>
+         <v-card class="heller">
+             <v-toolbar><v-avatar><img src="../assets/heller.png" alt=""></v-avatar><v-toolbar-title>Team Heller</v-toolbar-title></v-toolbar>
+           <v-container fluid grid-list-md> 
+            
+                <draggable v-model="heller" :element="'v-layout'" row wrap :options="{group:'team'}">
+                <v-flex class="heller" v-for="h in heller" :key="h.name">
+                    <div class="header">{{h.name}}</div>
+                    <img :src="h.img" height="150px" width="150px">
+                    <v-flex xs3>{{h.Information}}  {{h.Dauer}}</v-flex>
+                    
+                </v-flex>
+                 </draggable>
+             </v-container>
+         </v-card>
+     </v-flex>
+ </v-layout>
 
 
 
-
-
-    </v-card>
 </div>
     
 
@@ -38,6 +49,12 @@ export default {
                 {name:'Michele Grasso',Team:'Kärcher',Information:'Stamm',Dauer:'',img:'http://source.unsplash.com/random/150x150'},
                 {name:'Michael Gollhofer',Team:'Kärcher',Information:'Stamm',Dauer:'',img:'http://source.unsplash.com/random/150x150'},
                 {name:'Wolfgang Reichle',Team:'Kärcher',Information:'Temporär',Dauer:'12.6.2018-20.06.2018',img:'http://source.unsplash.com/random/150x150'},
+                {name:'Frank Valdez',Team:'Kärcher',Information:'Temporär',Dauer:'12.6.2018-20.06.2018',img:'http://source.unsplash.com/random/150x150'},
+                {name:'Sandra Lorey',Team:'Kärcher',Information:'Temporär',Dauer:'12.6.2018-20.06.2018',img:'http://source.unsplash.com/random/150x150'},
+            ],
+            heller:[
+                {name:'Marco Grossberger',Team:'Kärcher',Information:'Stamm',Dauer:'',img:'http://source.unsplash.com/random/130x130'},
+                
             ]
           
         }
@@ -54,4 +71,43 @@ export default {
     
 }
 </script>
+<style scoped>
+.card.kärcher{
+    background-color:yellow;
+    min-height:200px   
+}
+.card.heller{
+    background-color: #BBDEFB;
+        min-height:200px  
+}
+.flex.kärcher{
+    cursor:grab;
+}
+.flex.heller{
+    cursor:grab;
+}
+#kärchercss{
+       
+    display:inline-block;
+    height:160px;
+    width:160px;
+    cursor:grab;
+    
+    }
+.kärcherblock{
+   background-color:antiquewhite;
+   border:3px;
+   height:500px;
+   width:800px;
+   border-style:dashed 
+}    
+
+   
+#hellercss{
+    background-color:aquamarine
+}
+
+
+</style>
+
 
