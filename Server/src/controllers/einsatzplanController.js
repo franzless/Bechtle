e9afaf0bb0211be417a21f8bb77030edb751caf3
Brodbeck <AWX2341@AKAD.KAERCHER.COM>
2018-07-16 12:04:15 +0200

    implented filter query for users in backend

--- Server/src/controllers/einsatzplanController.js
+++ Server/src/controllers/einsatzplanController.js
@@ -3,12 +3,12 @@ const {teams} = require('../db/models')
 const {users} = require('../db/models')
 const {skills} = require('../db/models')
 const {userskills} = require('../db/models')
+const {sequelize} =require ('../db/models')
 
 module.exports = {
     queryusers(req,res){
-        console.log(req.body)
         var l = req.body.length
-        console.log(l)
+        
         switch(l){
             case 1:
             userskills.findAll(
@@ -19,6 +19,22 @@ module.exports = {
                 }
             )
             break;
+            case 2:
+            sequelize.query("SELECT * FROM userskills WHERE skillSkillid = ? AND userUserid IN(SELECT userUserid from userskills where skillSkillid = ?)",
+        {replacements:[req.body[0],req.body[1]],type:sequelize.QueryTypes.SELECT})
+            .then((results =>{
+                res.send(results)
+                console.log(results)
+            }))
+            break;
+            case 3:
+            sequelize.query("SELECT * FROM userskills WHERE skillSkillid = ? AND userUserid IN(SELECT userUserid from userskills where skillSkillid = ? AND userUserid IN(select userUserid from userskills where skillSkillid = ?))",
+        {replacements:[req.body[0],req.body[1], req.body[2]],type:sequelize.QueryTypes.SELECT})
+            .then((results =>{
+                res.send(results)
+            }))
+            break;
+            
             default:
             res.send({Error:'Too many Criterias'})
         }
